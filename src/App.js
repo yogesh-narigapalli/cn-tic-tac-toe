60a2ed84ec4ddf482e2b9e7b3ba8172a099f9ccc
unknown <yogesh@bizense.com>
2020-04-24 21:00:10 +0530

    Shaped out the game

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hey, Yogesh !</h1>
-      </header>
+    <div >
+<h1>Tic-Tac-Toe</h1>
+      <Game />
     </div>
+   
   );
 }
 
