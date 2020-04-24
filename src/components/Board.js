a62afc158401d687b13c5adcc41b127c17e1ccd9
unknown <yogesh@bizense.com>
2020-04-24 21:20:01 +0530

    tt

--- src/components/Board.js
+++ src/components/Board.js
@@ -15,12 +15,12 @@ export default class Board extends Component {
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </div>
-                <div className="border-row">
+                <div >
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                 </div>
-                <div className="border-row">
+                <div>
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
